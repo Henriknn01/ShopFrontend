8503e9c76213289cdbc15b4803642dfab3896b9b
Henrik <norheimhenrik@gmail.com>
2023-02-15 19:47:52 +0100

    Set up main page structure

---
+++ pages/about.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-white min-h-screen flex flex-col h-screen">
+    <ShopHeader/>
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex-grow grid grid-cols-1 gap-6 content-center">
+      <h1 class="text-4xl font-bold text-center py-4">ABOUT US</h1>
+      <p class="px-16 py-6">
+        We are Pet Plus, the premier online store for pet fashion and accessories.
+        Our mission is to provide pet owners with the highest quality pet clothing and accessories to keep their furry friends looking and feeling their best.
+        We believe that pets should be stylish and comfortable, and our products reflect that.
+        Our selection includes a wide range of pet clothing and accessories, such as coats,
+        sweaters, bandannas, and more.
+        <br>
+        <br>
+        We are committed to delivering the best shopping experience to our customers.
+        We take pride in our customer service and always strive to provide the highest level of satisfaction.
+        We also offer free shipping on orders over 750 NOK,
+        allowing you to stock up on pet fashion and accessories without breaking the bank.
+        <br>
+        <br>
+        We want to help make your pet look and feel their absolute best.
+        Shop with us today for the best selection of pet clothing and accessories.
+      </p>
+    </div>
+    <ShopFooter class="mt-auto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id]/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Product template {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
