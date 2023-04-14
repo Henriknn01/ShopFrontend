de5cf0323f0a3408fee84d95b2682d886b6e5619
Henrik <norheimhenrik@gmail.com>
2023-04-14 15:15:05 +0200

    implemented login with REST API

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,9 +1,61 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { NuxtAuthHandler } from "#auth";
 
+async function decodeToken(token) {
+    try {
+        const payloadBase64 = token.split('.')[1];
+        const decodedJson = Buffer.from(payloadBase64, 'base64').toString();
+        return JSON.parse(decodedJson);
+    } catch (error) {
+        console.warn(error)
+        return null
+    }
+}
+async function refreshAccessToken(refreshToken: {
+    accessToken: string;
+    accessTokenExpires: string;
+    refreshToken: string;
+}) {
+    try {
+        console.warn("trying to post to refresh token");
+        const refreshedTokensResponse = await fetch(
+            "http://127.0.0.1:8000/auth/jwt/refresh",
+            {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        refresh_token: refreshToken.refreshToken,
+                        mode: "json",
+                    }),
+            }
+        );
+        const refreshedTokens = await refreshedTokensResponse.json()
+
+        if (!refreshedTokens) {
+            console.warn("No refreshed tokens");
+            throw refreshedTokens;
+        }
+        console.warn("Refreshed tokens successfully");
+        return {
+            ...refreshToken,
+            accessToken: refreshedTokens.access,
+            accessTokenExpires: refreshedTokens.accessTokenExpires,
+            refreshToken: refreshedTokens.refresh,
+        };
+    } catch (error) {
+        console.warn("Error refreshing token", error);
+        return {
+            ...refreshToken,
+            error: "RefreshAccessTokenError",
+    };
+    }
+}
+
 export default NuxtAuthHandler({
     // secret needed to run nuxt-auth in production mode (used to encrypt data)
-    secret: process.env.NUXT_SECRET,
+    secret:  'sup3r-s3cr37',//process.env.NUXT_SECRET,
     pages: {
         // Change the default behavior to use `/login` as the path for the sign-in page
         signIn: '/login'
@@ -13,38 +65,94 @@ export default NuxtAuthHandler({
         CredentialsProvider.default({
             // The name to display on the sign in form (e.g. 'Sign in with...')
             name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
                 username: { label: 'Username', type: 'text', placeholder: 'username' },
                 password: { label: 'Password', type: 'password', placeholder: 'password' }
             },
-            authorize (credentials: any) {
-                // You need to provide your own logic here that takes the credentials
-                // submitted and returns either a object representing a user or value
-                // that is false/null if the credentials are invalid.
+            async authorize (credentials: any) {
+                try {
+                    const tokenResponse= await fetch(
+                        `http://127.0.0.1:8000/auth/jwt/create/`,
+                        {
+                                method: "POST",
+                                body: JSON.stringify({
+                                    email: credentials.username,
+                                    password: credentials.password,
+                                }),
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                        });
+                    const userTokens = await tokenResponse.json()
 
+                    const userDetailsResponse = await fetch(
+                        "http://127.0.0.1:8000/auth/users/me/", {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept-Language": "en-US",
+                            Authorization: `Bearer ${userTokens.access}`,
+                        },
+                    });
+                    const userDetails = await userDetailsResponse.json();
 
+                    if (!userTokens || !userDetails ) {
+                        throw createError({
+                            statusCode: 500,
+                            statusMessage: "Next auth failed",
+                        });
+                    };
+                    const decodedAccessJWT = await decodeToken(userTokens.access);
+                    const decodedRefreshJWT = await decodeToken(userTokens.access);
 
-                // NOTE: THE BELOW LOGIC IS NOT SAFE OR PROPER FOR AUTHENTICATION!
-
-                const user = { id: '1', name: 'J Smith', username: 'jsmith@gmail.com', password: 'hunter2', image: 'https://avatars.githubusercontent.com/u/25911230?v=4' }
-
-                if (credentials?.username === user.username && credentials?.password === user.password) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user
-                } else {
-                    // eslint-disable-next-line no-console
-                    console.error('Warning: Malicious login attempt registered, bad credentials provided')
-
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null
-
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                    const user = {
+                        id: userDetails.id,
+                        email: userDetails.email,
+                        firstName: userDetails.first_name,
+                        lastName: userDetails.last_name,
+                        role: userDetails.role,
+                        accessToken: userTokens.access,
+                        accessTokenExpires: decodedAccessJWT.exp,
+                        refreshToken: userTokens.refresh,
+                        refreshTokenExpires: decodedRefreshJWT.exp,
+                    };
+                    return user;
+                } catch (error) {
+                    console.warn("Error logging in", error);
+                    return null;
                 }
             }
         })
-    ]
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async jwt({ token, user, account, session }) {
+            if (account && user) {
+                console.warn("JWT callback", { token, user, account });
+                return {
+                    ...token,
+                    ...user,
+                };
+            }
+            // Handle token refresh before it expires of 15 minutes
+            if (token.accessTokenExpires && Date.now() > token.accessTokenExpires*1000) {
+                console.warn("Token is expired. Getting a new");
+                if (token.refreshTokenExpires && Date.now() > token.refreshTokenExpires*1000) {
+                    console.warn("Refresh token expired")
+                    // TODO: sign out when token expires.
+                }
+                return refreshAccessToken(token);
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = {
+                ...session.user,
+                ...token,
+            };
+            return session;
+        },
+    },
 })
