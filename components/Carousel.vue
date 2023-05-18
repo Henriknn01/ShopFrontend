f7e3541f772556fe14cbfedefb2d976ef11d6121
0xBACE <0xbace@gmail.com>
2023-05-18 12:47:55 +0200

    header backup

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto max-w-7xl py-6 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+  <div class="mx-auto max-w-7xl py-6 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8 z-20">
     <h2 class="text-2xl font-bold text-gray-900 mb-12">{{ title }}</h2>
     <div class="relative">
       <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 items-start overflow-hidden" style="max-width: 100%;">
