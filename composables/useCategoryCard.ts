74b23d404313d226db991f5d2e5257b99cf872e5
0xBACE <0xbace@gmail.com>
2023-05-21 22:37:59 +0200

    try catch on categories

--- composables/useCategoryCard.ts
+++ composables/useCategoryCard.ts
@@ -12,32 +12,36 @@ interface Category {
 
 
 export default async function useCategoryCard(categories: any[], catID: number | null): Promise<Category[]> {
-    const Categoryout: Category[] = [];
-    if (catID == "") {
-        catID = null;
-    }
-    for (const category of categories.value) {
-        if (category.parent_category == catID) {
-            if (category.image === null) {
-                Categoryout.push({
-                    id: category.id,
-                    name: category.name,
-                    imagesrc: null,
-                    imagealt: null,
-                    desc: category.desc,
-                    parent_category: category.parent_category,
-                });
-            } else {
-                Categoryout.push({
-                    id: category.id,
-                    name: category.name,
-                    imagesrc: category["image"].src,
-                    imagealt: category["image"].alt,
-                    desc: category.desc,
-                    parent_category: category.parent_category,
-                });
+    try {
+        const Categoryout: Category[] = [];
+        if (catID == "") {
+            catID = null;
+        }
+        for (const category of categories.value) {
+            if (category.parent_category == catID) {
+                if (category.image === null) {
+                    Categoryout.push({
+                        id: category.id,
+                        name: category.name,
+                        imagesrc: null,
+                        imagealt: null,
+                        desc: category.desc,
+                        parent_category: category.parent_category,
+                    });
+                } else {
+                    Categoryout.push({
+                        id: category.id,
+                        name: category.name,
+                        imagesrc: category["image"].src,
+                        imagealt: category["image"].alt,
+                        desc: category.desc,
+                        parent_category: category.parent_category,
+                    });
+                }
             }
         }
+        return Categoryout;
+    } catch (e){
+        console.warn(e)
     }
-    return Categoryout;
 }
