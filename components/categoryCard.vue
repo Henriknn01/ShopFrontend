0b7faa700d41bb4a1dd4fd3cf41903c991ddebb2
0xBACE <0xbace@gmail.com>
2023-05-23 23:20:14 +0200

    remvoed === made == to align with both null and values recived

--- components/categoryCard.vue
+++ components/categoryCard.vue
@@ -6,32 +6,33 @@ const props = defineProps({
 const items = ref([]);
 const config = useRuntimeConfig();
 let parent = props.parent
+console.log(parent)
 try {
     let {data: categories } = await useFetch(config.public.BACKEND_API_URL + `/productcategory/?parent_category=` + props.parent)
     if (props.parent === "") {
         parent = null
     }
     for (const category of await categories.value) {
-        if (category.parent_category === parent)  {
+        if (category.parent_category == parent)  {
             if (category.image === null) {
-            items.value.push({
-                id: category.id,
-                name: category.name,
-                imagesrc: null,
-                imagealt: null,
-                desc: category.desc,
-                parent_category: category.parent_category,
-            });
-        } else {
-            items.value.push({
-                id: category.id,
-                name: category.name,
-                imagesrc: category["image"].src,
-                imagealt: category["image"].alt,
-                desc: category.desc,
-                parent_category: category.parent_category,
-            });
-        }
+                items.value.push({
+                    id: category.id,
+                    name: category.name,
+                    imagesrc: null,
+                    imagealt: null,
+                    desc: category.desc,
+                    parent_category: category.parent_category,
+                });
+            } else {
+                items.value.push({
+                    id: category.id,
+                    name: category.name,
+                    imagesrc: category["image"].src,
+                    imagealt: category["image"].alt,
+                    desc: category.desc,
+                    parent_category: category.parent_category,
+                });
+            }
         }
     }
 } catch (e) {
