da55c6324b246a9f95a6ae6bbb8671e29c9029c7
Henrik <norheimhenrik@gmail.com>
2023-05-30 01:15:29 +0200

    changed date format for reviews

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -62,10 +62,8 @@ export default {
     methods: {
         formatDate(time) {
             const date = new Date(time);
-            const year = date.toLocaleString("default", { year: "numeric" });
-            const month = date.toLocaleString("default", { month: "2-digit" });
-            const day = date.toLocaleString("default", { day: "2-digit" });
-            return day+month+year;
+            const options = {  year: 'numeric', month: 'numeric', day: 'numeric'}
+            return date.toLocaleDateString('en-US', options);
         },
     }
 }
