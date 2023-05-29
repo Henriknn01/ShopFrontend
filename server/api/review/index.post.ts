2a2b5d6becf4160af7349ffee64b939e6ea2727d
Henrik <norheimhenrik@gmail.com>
2023-05-29 15:00:58 +0200

    Protected endpoint

--- server/api/review/index.post.ts
+++ server/api/review/index.post.ts
@@ -1,23 +1,30 @@
+import { getServerSession } from '#auth'
 export default defineEventHandler(async (event) => {
-    try {
-        const config = useRuntimeConfig();
-        const body = await readBody(event);
-        const response = await fetch(config.public.BACKEND_API_URL + "/productReview/", {
-            method: "POST",
-            body: JSON.stringify({
-                content: body.msg,
-                rating: body.rating,
-                product: body.product,
-                author: body.author,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": 'Bearer ' + body.token,
-            },
-        });
-        return response.status;
-    } catch (e) {
-        console.warn(e);
-        return null;
+    const session = await getServerSession(event)
+    if (!session) {
+        return { status: 'unauthenticated!'};
+    } else {
+        try {
+            const config = useRuntimeConfig();
+            const body = await readBody(event);
+            const response = await fetch(config.public.BACKEND_API_URL + "/productReview/", {
+                method: "POST",
+                body: JSON.stringify({
+                    content: body.msg,
+                    rating: body.rating,
+                    product: body.product,
+                    author: body.author,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": 'Bearer ' + body.token,
+                },
+            });
+            return response.status;
+        } catch (e) {
+            console.warn(e);
+            return null;
+        }
     }
+
 });
