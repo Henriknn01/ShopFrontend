9ef304d8c4d887a7ee2a04ce1b4c203758e4546a
Henrik <norheimhenrik@gmail.com>
2023-05-30 15:05:30 +0200

    added contact us page

---
+++ pages/contact.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="my-4">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex-grow grid grid-cols-1 gap-6 content-center">
+            <h1 class="text-4xl font-bold text-center py-4">CONTACT US</h1>
+
+            <div class="flex gap-8 mb-8">
+                <div class="flex-1">
+                    <h2 class="text-2xl font-semibold">Address</h2>
+                    <hr class="my-4">
+                    <div class="mb-8">
+                        <h4 class="font-semibold">Address</h4>
+                        <p>Larsgårdsvegen 2</p>
+                    </div>
+                    <div class="mb-8">
+                        <h4 class="font-semibold">Zip code</h4>
+                        <p>6009</p>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold">City</h4>
+                        <p>Ålesund</p>
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <h2 class="text-2xl font-semibold">Email</h2>
+                    <hr class="my-4">
+                    <div class="mb-8">
+                        <h4 class="font-semibold">Support</h4>
+                        <a href = "mailto: support@norheimweb.com">support@norheimweb.com</a>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold">Sales</h4>
+                        <a href = "mailto: sales@norheimweb.com">sales@norheimweb.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "contact"
+}
+</script>
+
+<style scoped>
+
+</style>
