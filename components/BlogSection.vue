37a9800195be0b2d5a9d00542b4f6fff960d581b
Henrik <norheimhenrik@gmail.com>
2023-05-30 13:55:45 +0200

    Made blog section into a component

---
+++ components/BlogSection.vue
@@ -0,0 +1,37 @@
+<script setup>
+const {init, visiblePosts} = await useBlog()
+await init()
+</script>
+
+<template>
+    <div class="mx-auto max-w-2xl lg:mx-0">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+        <p class="mt-2 text-lg leading-8 text-gray-600">Learn more about the latest news from Wave</p>
+    </div>
+    <div class="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <article v-for="(item, index) in visiblePosts" :key="index" class="flex max-w-xl flex-col items-start">
+            <div class="flex items-center gap-x-4 text-xs">
+                <time :datetime="item.date" class="text-gray-500">{{ item.date }}</time>
+            </div>
+            <div class="group relative">
+                <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a :href="/blog/ + item.id">
+                        <span class="absolute inset-0"></span>
+                        {{ item.title }}
+                    </a>
+                </h3>
+                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{{ item.content }}</p>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlogSection"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,5 +1,6 @@
 <script setup>
 import { StarIcon } from "@heroicons/vue/24/solid";
+import BlogSection from "../../components/BlogSection.vue";
 const route = useRoute();
 const config = useRuntimeConfig();
 definePageMeta({ auth: false });
@@ -44,8 +45,6 @@ if (p.value) {
         setSelectedImage(p.value.image[0].src, p.value.image[0].alt);
     }
 }
-const {init, visiblePosts} = await useBlog()
-await init()
 </script>
 
 <template>
@@ -87,26 +86,8 @@ await init()
             <Reviews :product-id="product.id.toString()"/>
 
             <CategoryCarousel title="More products like this..." :queryset="product.category.slice(-1)" limit="4"/>
-            <div class="mx-auto max-w-2xl lg:mx-0">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-                <p class="mt-2 text-lg leading-8 text-gray-600">Learn more about the latest news from Wave</p>
-            </div>
-            <div class="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-              <article v-for="(item, index) in visiblePosts" :key="index" class="flex max-w-xl flex-col items-start justify-between">
-                <div class="flex items-center gap-x-4 text-xs">
-                  <time :datetime="item.date" class="text-gray-500">{{ item.date }}</time>
-                </div>
-                <div class="group relative">
-                  <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a :href="/blog/ + item.id">
-                      <span class="absolute inset-0"></span>
-                      {{ item.title }}
-                    </a>
-                  </h3>
-                  <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{{ item.content }}</p>
-                </div>
-              </article>
-            </div>
+
+            <BlogSection/>
         </div>
     </div>
 </template>
